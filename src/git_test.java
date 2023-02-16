7e5c23c147a35a3f75b79265fb185251791cec8f
Song <Song@qq.com>
2023-02-16 11:58:07 +0800

    first commit----version3.0

--- src/git_test.java
+++ src/git_test.java
@@ -2,5 +2,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("first version--version1.0");
         System.out.println("first version--version2.0");
+        System.out.println("first version--version3.0");
     }
 }
