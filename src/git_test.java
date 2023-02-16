8cc0f9710fc66b117d365af721056d9d5c3d7ab7
Song <Song@qq.com>
2023-02-16 12:34:42 +0800

    hot-fix commit----version1.0

--- src/git_test.java
+++ src/git_test.java
@@ -3,5 +3,6 @@ public class git_test {
         System.out.println("first version--version1.0");
         System.out.println("first version--version2.0");
         System.out.println("first version--version3.0");
+        System.out.println("hot-fix version-- hot-fix version1.0");
     }
 }
